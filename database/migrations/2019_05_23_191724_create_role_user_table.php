c1d7977bff4a89cf3b1360ae682f5a5e0972d32b
Luiz Gustavo Xavier <luizxvr@hotmail.com>
2019-05-28 12:59:51 -0300

    Users_Roles funcional

--- database/migrations/2019_05_23_191724_create_role_user_table.php
+++ database/migrations/2019_05_23_191724_create_role_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAdminsTable extends Migration
+class CreateRoleUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateAdminsTable extends Migration
      */
     public function up()
     {
-        Schema::create('admins', function (Blueprint $table) {
+        Schema::create('role_user', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('nome');
-            $table->string('email')->unique();
-            $table->string('senha');
-            $table->rememberToken();
+            $table->integer('role_id');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateAdminsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admins');
+        Schema::dropIfExists('role_user');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -24,3 +26,13 @@ Route::post('/addPassenger', 'PassengerController@store')->name('passenger.store
 Route::get('/lista', 'PassengerController@index')->name('passenger.index');
 
 Route::post('/deletePassenger/{id}', 'PassengerController@destroy')->name('passenger.delete');
+
+
+//testar a funcionalidade role_user
+
+/*Route::get('/user/{id}/role', function($id){
+	$user = User::find($id)->roles()->orderBy('id','desc')->get();
+
+	return $user[0]->name;
+
+});*/
